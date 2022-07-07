11a02d7469fac97e3e95293059c45dce98a9b3fc
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-07-06 22:22:43 -0400

    pokemon js update gen 1 - 8 scrollbar

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,6 +1,15 @@
 const pokedexDiv = document.querySelector(".pokedex");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
+const gen1 = document.getElementById("gen1");
+const gen2 = document.getElementById("gen2");
+const gen3 = document.getElementById("gen3");
+const gen4 = document.getElementById("gen4");
+const gen5 = document.getElementById("gen5");
+const gen6 = document.getElementById("gen6");
+const gen7 = document.getElementById("gen7");
+const gen8 = document.getElementById("gen8");
+
 let generationshow = 1;
 
 function showPokemonGen(gen) {
@@ -12,7 +21,7 @@ function showPokemonGen(gen) {
     5: [494, 649],
     6: [650, 721],
     7: [722, 809],
-    8: [810, 905],
+    8: [810, 898],
   };
 
   const pokemonGenDefault = [1, 151];
@@ -22,12 +31,68 @@ function showPokemonGen(gen) {
 
 let pokemonGeneration = showPokemonGen(generationshow);
 
+gen1.addEventListener("click", () => {
+  generationshow = 1;
+  pokemonGeneration = showPokemonGen(generationshow);
+  removeChildNodes(pokedexDiv);
+  fetchPokemons();
+});
+
+gen2.addEventListener("click", () => {
+  generationshow = 2;
+  pokemonGeneration = showPokemonGen(generationshow);
+  removeChildNodes(pokedexDiv);
+  fetchPokemons();
+});
+
+gen3.addEventListener("click", () => {
+  generationshow = 3;
+  pokemonGeneration = showPokemonGen(generationshow);
+  removeChildNodes(pokedexDiv);
+  fetchPokemons();
+});
+
+gen4.addEventListener("click", () => {
+  generationshow = 4;
+  pokemonGeneration = showPokemonGen(generationshow);
+  removeChildNodes(pokedexDiv);
+  fetchPokemons();
+});
+
+gen5.addEventListener("click", () => {
+  generationshow = 5;
+  pokemonGeneration = showPokemonGen(generationshow);
+  removeChildNodes(pokedexDiv);
+  fetchPokemons();
+});
+
+gen6.addEventListener("click", () => {
+  generationshow = 6;
+  pokemonGeneration = showPokemonGen(generationshow);
+  removeChildNodes(pokedexDiv);
+  fetchPokemons();
+});
+
+gen7.addEventListener("click", () => {
+  generationshow = 7;
+  pokemonGeneration = showPokemonGen(generationshow);
+  removeChildNodes(pokedexDiv);
+  fetchPokemons();
+});
+
+gen8.addEventListener("click", () => {
+  generationshow = 8;
+  pokemonGeneration = showPokemonGen(generationshow);
+  removeChildNodes(pokedexDiv);
+  fetchPokemons();
+});
+
 prev.addEventListener("click", () => {
-  if (generationshow > 0){
-    generationshow -= 1
-    pokemonGeneration = showPokemonGen(generationshow)
+  if (generationshow > 0) {
+    generationshow -= 1;
+    pokemonGeneration = showPokemonGen(generationshow);
     removeChildNodes(pokedexDiv);
-    fetchPokemons()
+    fetchPokemons();
   }
 });
 
@@ -36,7 +101,7 @@ next.addEventListener("click", () => {
     generationshow += 1;
     pokemonGeneration = showPokemonGen(generationshow);
     removeChildNodes(pokedexDiv);
-    fetchPokemons()
+    fetchPokemons();
   }
 });
 
@@ -50,7 +115,7 @@ const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const rest = await fetch(url);
   const pokemon = await rest.json();
-  console.log(pokemon.id)
+  console.log(pokemon.id);
   createPokemon(pokemon);
 };
 
